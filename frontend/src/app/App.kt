232e5855949f224f1e931892ce7c04701d93020c
Johannes Preis <johannes.preis@comsysto.com>
2018-01-22 11:33:56 +0100

    clean slate for frontend

--- frontend/src/app/App.kt
+++ frontend/src/app/App.kt
@@ -1,46 +1,19 @@
 package app
 
-import logo.logo
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
-import react.dom.code
-import react.dom.div
 import react.dom.h2
-import react.dom.p
-import ticker.ticker
+import react.dom.header
 
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
-        div("App-header") {
+        header() {
             key = "header"
-            logo()
             h2 {
-                +"Welcome to React with Kotlin what up"
+                +"Kotlin Lab"
             }
-        }
-        p("App-intro") {
-            key = "intro"
-            +"To get started, edit "
-            code { +"app/App.kt" }
-            +" and save to reload."
-        }
-        p("App-ticker.ticker") {
-            key = "ticker"
-            +"This app has been running for "
-            ticker()
-            +" seconds."
-        }
-
-        div {
-            key = "statusbar"
-            statusBar("hello")
-        }
-
-        div {
-            key = "constbar"
-            constBar("Hello")
         }
 
     }
