6d78b62b6fb697baf601d869397577c3607f6057
Christian Maus <c.maus@reply.de>
2018-01-22 14:18:49 +0100

    use constructor injection

--- backend/src/main/kotlin/com/comsysto/kotlinfullstack/backend/routehandlers.kt
+++ backend/src/main/kotlin/com/comsysto/kotlinfullstack/backend/routehandlers.kt
@@ -1,17 +1,12 @@
 package com.comsysto.kotlinfullstack.backend
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
-import reactor.core.publisher.Flux
 
 @Component
-class RouteHandler {
-
-    @Autowired
-    lateinit var cryptoStockService:CryptoStockService
+class RouteHandler(private val cryptoStockService: CryptoStockService) {
 
     fun cryptoStockTicker(request: ServerRequest) = ServerResponse.ok()
             .contentType(MediaType.TEXT_EVENT_STREAM)
