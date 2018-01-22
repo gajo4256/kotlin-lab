c81ff5c20063c1388729d4048c653cee88dce8b5
Lyuben Dimitrov <lyuben.dimitrov.ext@pentasys.de>
2018-01-22 14:17:59 +0100

    Basic wiring with bootstrap and jquery

--- frontend/src/app/App.kt
+++ frontend/src/app/App.kt
@@ -5,23 +5,45 @@ import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
-import react.dom.div
-import react.dom.h2
-import react.dom.header
+import react.dom.*
 
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
-        header() {
+        nav(classes = "navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0") {
             key = "header"
+            a(classes = "navbar-brand col-sm-3 col-md-2 mr-0", href = "#") {
+                +"comSysto Kotlin Lab"
+            }
+
+            ul(classes = "navbar-nav px-3") {
+                li(classes = "nav-item text-nowrap") {
+                    a(classes = "nav-link", href = "#") {
+                        +"Sign Out"
+                    }
+                }
+            }
+        }
+
+        header() {
+            key = "title"
             h2 {
                 +"Kotlin Lab"
             }
         }
+
         div(classes = "currency-tiles") {
             currencyTile("Bitcoin", 9999)
             currencyTile("Ethereum", 700)
             currencyTile("Monero", 288)
+        }
 
+        footer(classes = "footer") {
+            key = "footer"
+            div(classes = "container") {
+                span(classes = "text-muted") {
+                    +"comSysto 2018"
+                }
+            }
         }
     }
 }
