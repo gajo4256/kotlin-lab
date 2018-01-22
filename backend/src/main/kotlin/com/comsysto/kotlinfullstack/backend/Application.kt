4297d042d3086ba7c50ae37ae744039e26a453d4
Tomislav Gajsak <tomislav.gajsak@comsysto.com>
2018-01-22 11:28:29 +0100

    Make starting of app more kotlin style

--- backend/src/main/kotlin/com/comsysto/kotlinfullstack/backend/Application.kt
+++ backend/src/main/kotlin/com/comsysto/kotlinfullstack/backend/Application.kt
@@ -1,14 +1,11 @@
 package com.comsysto.kotlinfullstack.backend
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.http.MediaType
-import org.springframework.web.reactive.function.server.router
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class ReactiveApplication
 
 fun main(args: Array<String>) {
-    SpringApplication.run(ReactiveApplication::class.java, *args)
+    runApplication<ReactiveApplication>(*args)
 }
