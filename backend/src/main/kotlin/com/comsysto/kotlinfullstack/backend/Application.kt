9347b85c010ff36588a6d7c34fded85a576dadb1
bensteinert <benjamin.steinert@comsysto.com>
2018-01-22 11:27:43 +0100

    import cleanup after config split

--- backend/src/main/kotlin/com/comsysto/kotlinfullstack/backend/Application.kt
+++ backend/src/main/kotlin/com/comsysto/kotlinfullstack/backend/Application.kt
@@ -2,9 +2,6 @@ package com.comsysto.kotlinfullstack.backend
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.http.MediaType
-import org.springframework.web.reactive.function.server.router
 
 @SpringBootApplication
 class ReactiveApplication
