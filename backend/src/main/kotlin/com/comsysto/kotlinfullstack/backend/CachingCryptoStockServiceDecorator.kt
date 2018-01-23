995e5c70a6f75a5fe950c975c90af281f9c665a2
bensteinert <benjamin.steinert@comsysto.com>
2018-01-23 14:56:25 +0100

    funs CachingCryptoStockServiceDecorator

--- backend/src/main/kotlin/com/comsysto/kotlinfullstack/backend/CachingCryptoStockServiceDecorator.kt
+++ backend/src/main/kotlin/com/comsysto/kotlinfullstack/backend/CachingCryptoStockServiceDecorator.kt
@@ -5,12 +5,6 @@ import reactor.core.publisher.Flux
 class CachingCryptoStockServiceDecorator(private val subject: CryptoStockServiceInterface) : CryptoStockServiceInterface by subject {
     private val streamMap: MutableMap<List<String>, Flux<CryptoStock>> = mutableMapOf()
 
-    override fun currentPriceStream(currencyKeys: List<String>): Flux<CryptoStock> {
-        val sortedKeys = currencyKeys.sorted()
-        if (!streamMap.containsKey(sortedKeys)) {
-            streamMap[sortedKeys] = subject.currentPriceStream(sortedKeys)
-        }
-
-        return streamMap[sortedKeys]!!
-    }
+    override fun currentPriceStream(currencyKeys: List<String>): Flux<CryptoStock> =
+            streamMap.getOrPut(currencyKeys.sorted(), {subject.currentPriceStream(currencyKeys)})
 }
