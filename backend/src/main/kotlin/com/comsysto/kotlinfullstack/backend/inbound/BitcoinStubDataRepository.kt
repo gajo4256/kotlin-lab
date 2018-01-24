95108f1d93156ff3ebc9f572d10a234da60acbfa
Tomislav Gajsak <tomislav.gajsak@comsysto.com>
2018-01-24 09:58:21 +0100

    Extract time delay for BTC into yaml

--- backend/src/main/kotlin/com/comsysto/kotlinfullstack/backend/inbound/BitcoinStubDataRepository.kt
+++ backend/src/main/kotlin/com/comsysto/kotlinfullstack/backend/inbound/BitcoinStubDataRepository.kt
@@ -1,13 +1,16 @@
 package com.comsysto.kotlinfullstack.backend.inbound
 
 import com.comsysto.kotlinfullstack.api.crypto.ReactiveClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
 import java.math.BigDecimal
 import java.time.Duration
 
 @Component
-class BitcoinStubDataRepository(private val client: ReactiveClient) : CurrencyDataRepository {
+class BitcoinStubDataRepository(private val client: ReactiveClient,
+                                @Value("\${inbound.channel.remote.btc}") val throttle: Long) : CurrencyDataRepository {
+    
     override fun currencyKey(): String {
         return "BTC"
     }
@@ -15,7 +18,7 @@ class BitcoinStubDataRepository(private val client: ReactiveClient) : CurrencyDa
     override fun dataStream(): Flux<BigDecimal> {
         return client.getSpotPrice(currencyKey())
                 .map { BigDecimal(it.data.amount) }
-                .delaySubscription(Duration.ofSeconds(1))
+                .delaySubscription(Duration.ofMillis(throttle))
                 .repeat()
     }
 }
