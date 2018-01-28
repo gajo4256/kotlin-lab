c337c265d3ee37f3afff6767f710af84daa0116f
Christian Maus <c.maus@reply.de>
2018-01-28 13:08:20 +0100

    changed subscription method on published subscriber to refcount so that the stream is cancelled if the last subscriber disconnects

--- backend/src/main/kotlin/com/comsysto/kotlinfullstack/backend/inbound/LitecoinStubDataRepository.kt
+++ backend/src/main/kotlin/com/comsysto/kotlinfullstack/backend/inbound/LitecoinStubDataRepository.kt
@@ -2,12 +2,14 @@ package com.comsysto.kotlinfullstack.backend.inbound
 
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.reactor.flux
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
 import java.math.BigDecimal
 
 @Component
-class LitecoinStubDataRepository : CurrencyDataRepository {
+class LitecoinStubDataRepository(
+        @Value("\${inbound.stubs.throttle.ltc}") val throttleValue: Long) : CurrencyDataRepository {
 
     override fun currencyKey(): String {
         return "LTC"
@@ -15,9 +17,8 @@ class LitecoinStubDataRepository : CurrencyDataRepository {
 
     override fun dataStream(): Flux<BigDecimal> {
         val flux = flux {
-            println("constructing datastream for ${currencyKey()}")
             while (true) {
-                delay(2000)
+                delay(throttleValue)
                 val result = BigDecimal(Math.random() * 4711)
                 send(result)
             }
